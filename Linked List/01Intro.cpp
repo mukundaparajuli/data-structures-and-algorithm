87edfa59b59fdedc92aace9ba56e2e88f3f4b29a
Mukunda Parajuli <mukundaparajuli13@gmail.com>
2024-05-02 23:42:37 +0545

    linkedlist

---
+++ Linked List/01Intro.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+class LinkedListNode{
+    public:
+        int data;
+        LinkedListNode* next;
+
+    LinkedListNode(int data){
+        this->data=data;
+        this->next=NULL;
+    }
+};
+int main(){
+    LinkedListNode* N1 =new LinkedListNode(10);
+    cout<<"Data=" <<N1->data<<endl;
+    cout<<"Next=" <<N1->next<<endl;
+
+    return 0;
+}
